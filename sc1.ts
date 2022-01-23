fdb3b8413a784cbb01b5a1dc8b362a662d75b663
tsukumon <syou.1225st@gmail.com>
2022-01-23 20:42:57 +0900

    section 4 end

--- sc1.ts
+++ sc1.ts
@@ -1,9 +1,51 @@
+//
 var x,kib:number;
 x = 2;
 kib = Math.pow(x,10);
 alert(kib);
 
+//演算子の結合方向
 var a,b:number;
 b = 10;
 a = b += 2; 
 alert("aの値は" + a + "\nbの値は" + b);
+
+//
+var a,b,c:number;
+b = 30;
+c = 2;
+a = b/c * 3;
+alert("aの値は" + a);
+
+//インクリメントとデクリメント
+var count:number = 0;
+alert(++count);
+
+var count:number = 0;
+alert(count++);
+
+var count:number = 0;
+count++;
+alert(count);
+
+var max:number = 10;
+alert(--max);
+
+//カンマ演算子
+var a:number = 10, b:number = 20;
+var c;
+c = (a + 1, b, 30);
+alert(c);
+
+//
+var age:number;
+age = 23;
+if(age>= 18 && age < 25){
+    alert('範囲内です');
+}
+
+//
+var x:number = 10;
+var y;
+y = x++;
+alert(y);
