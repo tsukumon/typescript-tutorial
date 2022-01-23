a63e5db779839600548d2ad8aacf64ccb5726a28
tsukumon <syou.1225st@gmail.com>
2022-01-23 23:38:24 +0900

    End sc5-2

--- sc5.ts
+++ sc5.ts
@@ -8,3 +8,18 @@ if(h < 12){
     c="lightyellow";
 }
 document.body.style.backgroundColor=c;
+
+var c,t:string;
+var h:number, d:Date;
+
+d=new Date();
+h=d.getHours();
+if(h<12){
+    t="午前のページ";
+    c="skyblue";
+}else{
+    t="午後のページ";
+    c="lightblue";
+}
+document.title=t;
+document.body.style.backgroundColor=c;
